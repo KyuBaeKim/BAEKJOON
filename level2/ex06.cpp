94742984a37ae76c7ddeaec837cd1ed8f1080278
kimkyubae <kkyub5118@gmail.com>
2022-03-06 16:37:47 +0900

    ex07

---
+++ level2/ex06.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+   int a =4;
+   int b = 7;
+   int c = a|b;
+
+   printf("%d", c);
+   return 0;
+}
