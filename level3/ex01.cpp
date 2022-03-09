9efa2aa42d905d1f770e63c35ff09ee66c631c2b
kimkyubae <kkyub5118@gmail.com>
2022-03-09 21:10:37 +0900

    구구단

--- level3/ex01.cpp
+++ level3/ex01.cpp
@@ -1,1 +1,17 @@
-//터렛 문제
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int x;
+    int total = 0;
+    cin >> x;
+    for (int i = 1; i < 10; i++)
+    {
+        total = x * i;
+        printf("%d * %d = %d\n", x, i, total);
+    }
+
+    return 0;
+}
