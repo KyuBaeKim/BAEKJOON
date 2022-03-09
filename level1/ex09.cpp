a623d9315ef6819944b621802fcc5e4a6a25a640
kimkyubae <kkyub5118@gmail.com>
2022-03-09 21:04:30 +0900

    터렛

--- level1/ex09.cpp
+++ level1/ex09.cpp
@@ -4,6 +4,33 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-   
-   return 0;
+    int T;
+    int x1, y1, r1, x2, y2, r2;
+    int d, cond1, cond2;
+    cin >> T;
+
+    for (int test = 0; test < T; test++)
+    {
+        cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+        d = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+        cond1 = (r1 - r2) * (r1 - r2);
+        cond2 = (r1 + r2) * (r1 + r2);
+
+        if (d == 0)
+        {
+            if (cond1 == 0)
+                cout << "-1" << endl;
+            else
+                cout << "0" << endl;
+        }
+        else if (d == cond1 || d == cond2)
+            cout << "1" << endl;
+        else if (cond1 < d && d < cond2)
+            cout << "2" << endl;
+        else
+            cout << "0" << endl;
+
+    }
+
+return 0;
 }
